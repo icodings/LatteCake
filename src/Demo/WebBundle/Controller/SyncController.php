8428bf0cb10051cc1bd249c87ce0bc962d538aca
icowan <wack1001@163.com>
2015-01-12 20:05:03 +0800

    添加多说的同步本地数据

--- src/Demo/WebBundle/Controller/SyncController.php
+++ src/Demo/WebBundle/Controller/SyncController.php
@@ -49,65 +49,17 @@ class SyncController extends Controller
         $action     = $request->get('action');
         $signature  = $request->get('signature');
 
-        $url = "http://api.duoshuo.com/log/list.json";
+        $url = "http://api.duoshuo.com/log/list.json?short_name=lattecake&secret=c9d92470f13da30f2da8f48a4f4e167a";
 
         $response = $this->getUrlContent($url);
-print_r( $response );die;
 
+        print_r( json_decode($response, true) );die;
+
+        /*
         // Create a client to work with the Twitter API
         $client = new Client();
-
         $req = $client->get('http://api.duoshuo.com/log/list.json', ['verify' => false]);
-
-        print_r( $req );die;
-
-// Sign all requests with the OauthPlugin
-//        $client->addSubscriber(new Guzzle\Plugin\Oauth\OauthPlugin(array(
-//            'consumer_key'  => '***',
-//            'consumer_secret' => '***',
-//            'token'       => '***',
-//            'token_secret'  => '***'
-//        )));
-
-        echo $client->get('statuses/user_timeline.json')->send()->getBody();
-// >>> {"public_gists":6,"type":"User" ...
-
-// Create a tweet using POST
-        $request = $client->post('statuses/update.json', null, array(
-            'status' => 'Tweeted with Guzzle, http://guzzlephp.org'
-        ));
-
-// Send the request and parse the JSON response into an array
-        $data = $request->send()->json();
-        echo $data['text'];
-// >>> Tweeted with Guzzle, http://t.co/kngJMfRk
-die;
-
-
-
-        $result = array
-        (
-            'verify'    => false,
-//            'action'    => 'sync_log',
-//            'signature' => 'c9d92470f13da30f2da8f48a4f4e167a'
-        );
-
-//        $client = new Client([
-//            'base_url' => [
-//                'http://api.duoshuo.com/{log}/',
-//                ['log' => 'log']
-//            ],
-//            'defaults' => [
-//                'timeout'         => 100,
-//                'allow_redirects' => false,
-//                'proxy'           => $request->getClientIp()
-//            ]
-//        ]);
-        $client = new Client();
-        $response = $client->get('http://api.duoshuo.com/log/list.json', $result);
-
-        print_r($response->json());
-die;
+        */
 
         $logger->info(__CLASS__.'|'.__FUNCTION__."|action={$action}|signature={$signature}");
         return new JsonResponse($response);
