e36358e5ee647e854ece5bfde7d10c33de2ca3ac
icowan <wack1001@163.com>
2015-01-12 17:01:40 +0800

    添加多说的同步本地数据

--- src/Demo/WebBundle/Controller/SyncController.php
+++ src/Demo/WebBundle/Controller/SyncController.php
@@ -29,9 +29,13 @@ class SyncController extends Controller
      */
     public function commentsAction( Request $request )
     {
+        $logger = $this->get('logger');
         // http://api.duoshuo.com/log/list.json?short_name=api&secret=c9d92470f13da30f2da8f48a4f4e167a
         $action     = $request->get('action');
         $signature  = $request->get('signature');
+
+        $url = "http://api.duoshuo.com/log/list.json?short_name=api&secret=c9d92470f13da30f2da8f48a4f4e167a";
+
 
         $response = array
         (
@@ -39,6 +43,13 @@ class SyncController extends Controller
             'signature' => $signature
         );
 
+
+
+        $client = '';
+
+        $crawler = $client->request('GET', $url);
+
+        $logger->info(__CLASS__.'|'.__FUNCTION__."|action={$action}|signature={$signature}");
         return new JsonResponse($response);
     }
 } 
